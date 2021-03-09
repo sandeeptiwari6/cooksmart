3dc8ac31c7c85915d68a6753049d3827b6b88e2b
marcm97 <marc.mascarenhas20@gmail.com>
2021-03-09 11:24:41 -0800

    completed code coverage, did most of the linting

--- src/recommendation.py
+++ src/recommendation.py
@@ -25,13 +25,12 @@ urls = {
 }
 
 
-
-
 class RecipeRecommender:
     def __init__(self, filepath=None, max_df=0.6, min_df=2):
         """
         Creates an instance of Recipe Recommender
-        Initialises : filepath, tfidf_vect, data, recipe_ingredient_matrix, title_tfidf
+        Initialises : filepath, tfidf_vect, data, recipe_ingredient_matrix, 
+        title_tfidf
         Reads in formatted csv file and vectorize recipes
         Input:
         filepath (str)
@@ -42,62 +41,67 @@ class RecipeRecommender:
         """
 
         self.filepath = filepath
-        self.tfidf_vect = TfidfVectorizer(max_df=max_df, min_df=min_df, stop_words='english')
+        self.tfidf_vect = TfidfVectorizer(max_df=max_df, min_df=min_df,
+                                          stop_words='english')
         if self.filepath is None:
             self.data = pd.read_csv('../data/cleaned-data_recipe.csv')
             with open('pickles/recipe_topics.pickle', 'rb') as f:
                 self.recipe_ingredient_matrix = pickle.load(f)
             with open('pickles/vectorizer.pickle', 'rb') as f:
                 self.tfidf_vect = pickle.load(f)
-            self.title_tfidf = self.tfidf_vect.transform(self.data['recipe_name'])
+            self.title_tfidf = self.tfidf_vect.transform(
+                self.data['recipe_name'])
 
         else:
             if not os.path.exists(filepath):
-                raise FileNotFoundError(f"{filepath} is not a valid path to a dataset")
+                raise FileNotFoundError(
+                    f"{filepath} is not a valid path to a dataset")
             self.data = pd.read_csv(filepath)
-            
+       
             if not is_valid_recipe_df(self.data):
                 raise DataFormatError("Inputted csv is incorrectly formatted")
 
-            self.recipe_ingredient_matrix = self.tfidf_vect.fit_transform(self.data['ingredients'].values.astype('U'))
-            self.title_tfidf = self.tfidf_vect.transform(self.data['recipe_name'])
-
+            self.recipe_ingredient_matrix = self.tfidf_vect.fit_transform(
+                self.data['ingredients'].values.astype('U'))
+            self.title_tfidf = self.tfidf_vect.transform(
+                                            self.data['recipe_name'])
 
     def fit(self, n_components=10):
         """
         Fits an LDA model to the dataset provided
-        For each recipe, creates a topic distribution for the ingredients as well as for the recipe name
+        For each recipe, creates a topic distribution for the ingredients as
+        well as for the recipe name
 
         Input: n_components (INT): number of topics desired
 
         Output: None
         """
-
-        self.n_components= n_components
-        if self.filepath is None and self.n_components==10:
-            with open ('pickles/lda.pickle','rb') as f:
+        self.n_components = n_components
+        if self.filepath is None and self.n_components == 10:
+            with open('pickles/lda.pickle', 'rb') as f:
                 self.LDA = pickle.load(f)
             with open('pickles/title_topics.pickle', 'rb') as f:
                 self.title_topic_dist = pickle.load(f)
-            #save as pickle
-            self.recipe_topic_dist = np.matrix(self.LDA.transform(self.recipe_ingredient_matrix))
-            
-        else:   
-            self.LDA = LatentDirichletAllocation(n_components=n_components, random_state=42)
+            # save as pickle
+            self.recipe_topic_dist = np.matrix(self.LDA.transform(
+                                                self.recipe_ingredient_matrix))
+        else:
+            self.LDA = LatentDirichletAllocation(n_components=n_components,
+                                                 random_state=42)
             self.LDA.fit_transform(self.recipe_ingredient_matrix)
-            self.recipe_topic_dist = np.matrix(self.LDA.transform(self.recipe_ingredient_matrix))
+            self.recipe_topic_dist = np.matrix(self.LDA.transform(
+                                                self.recipe_ingredient_matrix))
             self.title_topic_dist = self.LDA.transform(self.title_tfidf)
 
     def recipe_similarity(self, w_title=0.2, w_text=0.3):
         """
-        Compares topic distribution of input ingredients against topic 
+        Compares topic distribution of input ingredients against topic
         distribution of each recipe in dataset, calculates "similarity"
 
-        Input: 
+        Input:
         w_title (float between 0 and 1)
         w_text (float between 0 and 1)
-
-        Output: 
+        Output:
         scores (np.array)
         """
         scores = self.recipe_topic_dist * self.input_ingredients_topic_dist.T * w_text
@@ -123,42 +127,42 @@ class RecipeRecommender:
         if len(input_ingredients) < 5:
             raise QueryError("Input atleast 5 Ingredients")
 
-
-        input_ingredients_tfidf = self.tfidf_vect.transform([' '.join(input_ingredients)])
-        self.input_ingredients_topic_dist = np.matrix(self.LDA.transform(input_ingredients_tfidf))
+        input_ingredients_tfidf = self.tfidf_vect.transform(
+                                                [' '.join(input_ingredients)])
+        self.input_ingredients_topic_dist = np.matrix(self.LDA.transform(
+                                                input_ingredients_tfidf))
         scores = self.recipe_similarity()
         sorted_index = np.argsort(scores)[::-1]
         return self.data.iloc[sorted_index, :].head(n)
 
+    def visualize_fit(self):
+        topic_viz = pyLDAvis.sklearn.prepare(self.LDA,
+                                             self.recipe_ingredient_matrix,
+                                             self.tfidf_vect)
+        save_file = 'lda-results.html'
+        pyLDAvis.save_html(topic_viz, save_file)
+        webbrowser.open('file://' + os.path.realpath(save_file))
+        time.sleep(1)
+        os.remove(save_file)
 
-    # def visualize_fit(self):
-    #     topic_viz = pyLDAvis.sklearn.prepare(self.LDA, self.recipe_ingredient_matrix, self.tfidf_vect)
-    #     save_file = 'lda-results.html'
-    #     pyLDAvis.save_html(topic_viz, save_file)
-    #     webbrowser.open('file://' + os.path.realpath(save_file))
-    #     time.sleep(1)
-    #     os.remove(save_file)
+    def visualize_recommendation(self):
+        try:
+            topic_dist = np.squeeze(np.asarray(
+                self.input_ingredients_topic_dist))
+            topics = [f"Topic {i}" for i in range(1, len(topic_dist)+1)]
+            df = pd.DataFrame({'topics': topics, 'probability': topic_dist})
+            fig = px.line_polar(df, r="probability", theta="topics",
+                                line_close=True, color_discrete_sequence=
+                                px.colors.sequential.Plasma_r,
+                                template="plotly_dark")
+            fig.show()
+        except AttributeError:
+            raise AttributeError(" Call `get_recommendations` first")
 
-    # def visualize_recommendation(self):
-    #     try:
-    #         topic_dist = np.squeeze(np.asarray(self.input_ingredients_topic_dist))
-    #         topics = [f"Topic {i}" for i in range(1, len(topic_dist)+1)]
-    #         df = pd.DataFrame({'topics': topics, 'probability': topic_dist})
-    #         fig = px.line_polar(df, r="probability", theta="topics", line_close=True,
-    #                       color_discrete_sequence=px.colors.sequential.Plasma_r, template="plotly_dark")
-    #         fig.show()
-    #     except AttributeError:
-    #         raise AttributeError("User must first call class method `get_recommendations` before viewing visualization")
-
-    # def pickle_model(self, lda_file="pickles/lda.pickle"):
-    #     with open(lda_file, "wb") as f:
-    #         print(f"Saving LDA model to {lda_file}...")
-    #         pickle.dump(self.LDA, f)
-
-
-        
-
-
+    def pickle_model(self, lda_file="pickles/lda.pickle"):
+        with open(lda_file, "wb") as f:
+            print(f"Saving LDA model to {lda_file}...")
+            pickle.dump(self.LDA, f)
 # if __name__ == "__main__":
 #     rr = RecipeRecommender()
 #     rr.fit()
@@ -167,6 +171,3 @@ class RecipeRecommender:
 #     rr.get_recommendations(query)
 #     # rr.visualize_fit()
 #     # rr.visualize_recommendation()
-
-
-
--- tests/test_recommendation.py
+++ tests/test_recommendation.py
@@ -1,123 +1,169 @@
 import unittest
 from recommendation import RecipeRecommender as rr
 from helpers import DataFormatError, QueryError
-from unittest.mock import Mock
+from unittest.mock import Mock, patch
 import os
 import pandas as pd
 import numpy as np
 
+
 class test_rr_initialization(unittest.TestCase):
 
-    #invalid file path
+    # invalid file path
     def test_filepath(self):
-        self.assertRaises(FileNotFoundError,rr,"invalid.csv")
+        self.assertRaises(FileNotFoundError, rr, "invalid.csv")
 
-    #invalid column names
+    # invalid column names
     def test_invalid_format(self):
-        data = {'recipe_name':['Chicken dish', 'pasta'], 
-        'ingredients':["chiecken", "pasta"]}
-        data =pd.DataFrame(data)
-        data.to_csv("invalid_data_format.csv") 
-        self.assertRaises(DataFormatError,rr,"invalid_data_format.csv")
+        data = {'recipe_name': ['Chicken dish', 'pasta'],
+                'ingredients': ["chiecken", "pasta"]}
+        data = pd.DataFrame(data)
+        data.to_csv("invalid_data_format.csv")
+        self.assertRaises(DataFormatError, rr, "invalid_data_format.csv")
         os.remove('invalid_data_format.csv')
-
 
     # title tfidf is correct size?
     def test_recipe_ingredient_matrix(self):
         r = rr()
-        self.assertEqual(len(r.data),r.recipe_ingredient_matrix.shape[0])
+        self.assertEqual(len(r.data), r.recipe_ingredient_matrix.shape[0])
 
-        r =rr('../data/cleaned-data_recipe.csv')
-        self.assertEqual(len(r.data),r.recipe_ingredient_matrix.shape[0])
-    
+        r = rr('../data/cleaned-data_recipe.csv')
+        self.assertEqual(len(r.data), r.recipe_ingredient_matrix.shape[0])
+
     def test_title_tfidf(self):
         r = rr()
-        self.assertEqual(len(r.data),r.title_tfidf.shape[0])
+        self.assertEqual(len(r.data), r.title_tfidf.shape[0])
 
-        r =rr('../data/cleaned-data_recipe.csv')
-        self.assertEqual(len(r.data),r.title_tfidf.shape[0])
+        r = rr('../data/cleaned-data_recipe.csv')
+        self.assertEqual(len(r.data), r.title_tfidf.shape[0])
 
 
 class test_rr_fit(unittest.TestCase):
-    #stay with defaults - fit shouldn't be called
-    def test_defaults(self):
-        pass
 
-    #provide filepath - fit should be called once
+    # provide filepath - LDA should be called once
+    @patch('recommendation.LatentDirichletAllocation')
+    def test_LDA_calls1(self, mock_A):
+        r = rr('../data/cleaned-data_recipe.csv')
+        r.fit()
+        self.assertEqual(mock_A.call_count, 1)
 
-    #change default num topics - fit should be called once
+    # provide no filepath - LDA should be called zero times
+    @patch('recommendation.LatentDirichletAllocation')
+    def test_LDA_calls2(self, mock_A):
+        r = rr()
+        r.fit()
+        self.assertEqual(mock_A.call_count, 0)
 
-    #recipe topic distribution
+    # change default num topics - LDA should be called once
+    @patch('recommendation.LatentDirichletAllocation')
+    def test_LDA_calls3(self, mock_A):
+        r = rr()
+        r.fit(n_components=5)
+        self.assertEqual(mock_A.call_count, 1)
+
+    # recipe topic distribution
     def test_recipe_topic_distribution(self):
-        r =rr()
+        r = rr()
         r.fit()
-        self.assertEqual(r.n_components,r.recipe_topic_dist.shape[1])
-        self.assertEqual(len(r.data),r.recipe_topic_dist.shape[0])
-    #title topic distribution
+        self.assertEqual(r.n_components, r.recipe_topic_dist.shape[1])
+        self.assertEqual(len(r.data), r.recipe_topic_dist.shape[0])
+
+    # title topic distribution
     def test_title_topic_distribution(self):
-        r =rr()
+        r = rr()
         r.fit()
-        self.assertEqual(r.n_components,r.title_topic_dist.shape[1])
-        self.assertEqual(len(r.data),r.title_topic_dist.shape[0])
+        self.assertEqual(r.n_components, r.title_topic_dist.shape[1])
+        self.assertEqual(len(r.data), r.title_topic_dist.shape[0])
+
 
 class test_rr_get_recommendations(unittest.TestCase):
 
     def test_input_type(self):
-        r =rr()
+        r = rr()
         r.fit()
 
         with self.assertRaises(QueryError):
-            rr.get_recommendations(self,["1",3])
-
+            rr.get_recommendations(self, ["1", 3])
 
     def test_input_count(self):
-        r =rr()
+        r = rr()
         r.fit()
 
         with self.assertRaises(QueryError):
-            rr.get_recommendations(self,["1","2"])
+            rr.get_recommendations(self, ["1", "2"])
 
-    # check size of ingredient topic dist
+    @patch("recommendation.RecipeRecommender.recipe_similarity")
+    def test_similarity_call(self, mock_similarity):
+        r = rr()
+        r.fit()
+        r.get_recommendations(["pepper", "chicken", "salt", "vinegar",
+                              "tomato", "cheese"])
+        self.assertEqual(mock_similarity.call_count, 1)
 
-    #length of sorted index
-
-    # maek sure # of recommendations =n
-
-    #make sure that scores are descending
-    # def test_sorted_order(self):
-    #     r=rr()
-    #     r.fit()
-
+    def test_output(self):
+        r = rr()
+        r.fit()
+        scores = r.get_recommendations(["pepper", "chicken", "salt", "vinegar",
+                                        "tomato", "cheese"], 8)
+        self.assertEqual(len(scores), 8)
 
 
 class test_rr_similarity(unittest.TestCase):
 
     def test_score_array(self):
-        r=rr()
-        r.recipe_topic_dist = np.matrix([[0.3,0.2,0.2,0.1,0.2],
-                                         [0.2,0.1,0.4,0.3,0],
-                                         [0,0.5,0.1,0.4,0]])
-        r.title_topic_dist = np.matrix([[0.4,0.1,0.1,0.2,0.2],
-                                         [0.15,0.15,0.4,0.3,0],
-                                         [0,0.4,0.2,0.4,0]])
+        r = rr()
+        r.recipe_topic_dist = np.matrix([[0.3, 0.2, 0.2, 0.1, 0.2],
+                                         [0.2, 0.1, 0.4, 0.3, 0],
+                                         [0, 0.5, 0.1, 0.4, 0]])
+        r.title_topic_dist = np.matrix([[0.4, 0.1, 0.1, 0.2, 0.2],
+                                        [0.15, 0.15, 0.4, 0.3, 0],
+                                        [0, 0.4, 0.2, 0.4, 0]])
 
-        r.input_ingredients_topic_dist = np.matrix([[0.3,0.2,0.2,0.1,0.2]])
+        r.input_ingredients_topic_dist = np.matrix([[0.3, 0.2, 0.2, 0.1, 0.2]])
 
         score = r.recipe_similarity()
         self.assertEqual(len(score), 3)
-                            
+
+
+class test_rr_visualize_fit(unittest.TestCase):
+    # pyLDAvis should be called once
+    @patch('recommendation.pyLDAvis.sklearn.prepare')
+    @patch('recommendation.pyLDAvis.save_html')
+    @patch('recommendation.os.remove')
+    @patch('recommendation.webbrowser')
+    def test_pyLDAvis_calls1(self, mock_web, mock_os, mock_save, mock_prepare):
+        r = rr()
+        r.fit()
+        r.visualize_fit()
+        self.assertEqual(mock_prepare.call_count, 1)
+        self.assertEqual(mock_save.call_count, 1)
+        self.assertEqual(mock_os.call_count, 1)
 
 
 class test_rr_visualize_recommendation(unittest.TestCase):
     def test_visualize_call(self):
-        r=rr()
+        r = rr()
         with self.assertRaises(AttributeError):
             r.visualize_recommendation()
 
-class test_rr_pickle(unittest.TestCase):
-    def test_pickle(self):
-        r=rr()
+    @patch('recommendation.px.line_polar')
+    def test_fig_show(self, mock_show):
+        r = rr()
+        r.fit()
+        r.get_recommendations(["pepper", "chicken", "salt", "vinegar",
+                               "tomato", "cheese"])
+        mock_show.return_value = Mock()
+        r.visualize_recommendation()
+        self.assertEqual(mock_show.call_count, 1)
 
+
+class test_rr_pickle(unittest.TestCase):
+    @patch('recommendation.pickle.dump')
+    def test_pickle(self, mock_dump):
+        r = rr()
+        r.LDA = Mock()
+        r.pickle_model("test.pkl")
+        self.assertEqual(mock_dump.call_count, 1)
 
 
 if __name__ == '__main__':
