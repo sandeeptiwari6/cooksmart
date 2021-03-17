efb30916fa9dcf94a09956e5aa4e58d46729f7d8
Sandeep <sandeeptiwari6@users.noreply.github.com>
2021-03-16 20:00:48 -0700

    fixed setup.py

--- setup.py
+++ setup.py
@@ -4,16 +4,16 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="test-cooksmart",
+    name="cooksmart",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Sandeep Tiwari",
+    author_email="sandy972@uw.edu",
+    description="Package for recommending recipes",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/sandeeptiwari6/cooksmart",
     project_urls={
-        "Bug Tracker": "https://github.com/pypa/sampleproject/issues",
+        "Bug Tracker": "https://github.com/sandeeptiwari6/cooksmart/issues",
     },
     classifiers=[
         "Programming Language :: Python :: 3",
